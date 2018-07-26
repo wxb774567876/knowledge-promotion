64ca94adedc8a077656406424eb1a733e325d258
wxb774567876 <wxb774567876@163.com>
2018-07-27 00:40:34 +0800

    完善线程的生产者和消费者模式，加深对wait，notify,notifyAll的用法理解

--- java-multithreading/src/main/java/com/xianbing/a008_Thread_Wait_Notify_NotifyAll/Thread_Wait_Notify_NotifyAllDemo.java
+++ java-multithreading/src/main/java/com/xianbing/a008_Thread_Wait_Notify_NotifyAll/Thread_Wait_Notify_NotifyAllDemo.java
@@ -10,21 +10,27 @@ class Product {
     }
 
     public synchronized void produce(int count) {
+        String threadName = Thread.currentThread().getName();
         while (count > 0) {
-            String threadName = Thread.currentThread().getName();
-            System.out.print("【" + threadName + "】运行，想生产的总产品数量：" + count + ";");
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.print("【" + threadName + "】运行，需一次性生产总产品数量：" + count + ";");
             if (size >= maxSize) {
-                System.out.println("【此次生产的产品数量】：" + count + "，【库存量】：" + size + "，超出仓库总容量,生产任务无法执行！");
+                System.out.println("失败了，【此次想要生产的产品数量】：" + count + "，【库存量】：" + size + "，超出仓库总容量,生产任务无法执行！");
                 try {
                     wait();
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
+
             int produceCount = (size + count) > maxSize ? maxSize - size : count;
             size += produceCount;
             count -= produceCount;
-            System.out.println("【此次生产的产品数量】：" + produceCount + "，【当前仓库库存量】：" + size + "，唤醒其他任务开始执行！！！");
+            System.out.println("【此次生产的产品数量】：" + produceCount + "，【生产后仓库库存量】：" + size + "，唤醒其他任务开始执行！！！");
             notifyAll();
         }
 
@@ -32,10 +38,15 @@ class Product {
 
     public synchronized void consume(int count) {
         String threadName = Thread.currentThread().getName();
-        System.out.print("【" + threadName + "】运行，想消费的总产品数量：" + count + ";");
         while (count > 0) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.print("【" + threadName + "】运行，需一次性消费总产品数量：" + count + ";");
             if (size <= 0) {
-                System.out.println("【此次消费的产品数量】：" + count + "，【库存量】：" + size + "，没有可供消费的产品，消费任务暂停！");
+                System.out.println("失败了，【此次想要消费的产品数量】：" + count + "，【库存量】：" + size + "，没有可供消费的产品，消费任务暂停！");
                 try {
                     wait();
                 } catch (InterruptedException e) {
@@ -45,7 +56,7 @@ class Product {
             int consumeCount = (count - size) > 0 ? size : count;
             size -= consumeCount;
             count -= consumeCount;
-            System.out.println("【此次消费的产品数量】：" + count + "，【当前仓库库存量】：" + size + "，此次消费结束，" +
+            System.out.println("【此次消费的产品数量】：" + consumeCount + "，【消费后的仓库库存量】：" + size + "，此次消费结束，" +
                     "唤醒其他任务开始执行！！！");
             notifyAll();
         }
@@ -60,13 +71,14 @@ class Producer {
         this.product = product;
     }
 
-    public void produce(final int count) {
+    public void produce(final int count, String threadName) {
         new Thread(new Runnable() {
             @Override
             public void run() {
                 product.produce(count);
+                System.out.println(Thread.currentThread().getName() + "运行结束了！！！");
             }
-        }, "生产者").start();
+        }, "生产者" + threadName).start();
     }
 }
 
@@ -77,13 +89,14 @@ class Consumer{
         this.product = product;
     }
 
-    public void consume(final int count) {
+    public void consume(final int count, String threadName) {
         new Thread(new Runnable() {
             @Override
             public void run() {
                 product.consume(count);
+                System.out.println(Thread.currentThread().getName() + "运行结束了！！！");
             }
-        }, "消费者").start();
+        }, "消费者" + threadName).start();
     }
 
 }
@@ -93,17 +106,19 @@ public class Thread_Wait_Notify_NotifyAllDemo {
         Producer producerThread = new Producer(product);
         Consumer consumerThread = new Consumer(product);
 
-        consumerThread.consume(10);
+        consumerThread.consume(10, "XFZ_A");
 
-        producerThread.produce(110);
+        producerThread.produce(110, "SCZ_A");
 
-        consumerThread.consume(50);
-        consumerThread.consume(70);
-        consumerThread.consume(20);
-        consumerThread.consume(100);
+        consumerThread.consume(50, "XFZ_B");
+        consumerThread.consume(70, "XFZ_C");
+        consumerThread.consume(20, "XFZ_D");
+        consumerThread.consume(100, "XFZ_E");
 
-        producerThread.produce(60);
-        producerThread.produce(10);
-        producerThread.produce(80);
+        producerThread.produce(60, "SCZ_B");
+        producerThread.produce(10, "SCZ_C");
+        producerThread.produce(80, "SCZ_D");
+
+        //consumerThread.consume(50, "XFZ_F");
     }
 }
